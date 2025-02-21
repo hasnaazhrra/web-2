4f0e8d735c7074065ac7e13da7ff8a3a2491e18c
hasnaazhrra <hasnaazzahera@gmail.com>
2025-02-21 14:07:41 +0700

    praktikum 1

--- README.md
+++
@@ -1,8 +0,0 @@
-# Pemrograman web 2 
-## Repositori Praktikum Pemrograman Web 2
-
-- Nama : Hasna Azzahra
-- NIM : 0110124219
-- Rombel : SI10
-
-
---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+//mendefinisikan variable                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+$nama = 'hasna';
+$umur = 20;
+$berat = 50.5;
+
+echo 'nama saya ' . $nama;
+echo '<br/>umur : ' . $umur . ' tahun';
+echo '<br/>berat : ' . $berat
+
+?>
