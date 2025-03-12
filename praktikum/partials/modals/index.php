8185a37e63ca88abb0c843a6d35d4f79bfbf9dcc
hasnaazhrra <hasnaazzahera@gmail.com>
2025-03-13 04:53:45 +0700

    Menambahkan file ke branch praktikum03

---
+++ praktikum/partials/modals/index.php
@@ -0,0 +1,7 @@
+<?php
+include_once "portofolio-1.php";
+include_once "portofolio-2.php";
+include_once "portofolio-3.php";
+include_once "portofolio-4.php";
+include_once "portofolio-5.php";
+include_once "portofolio-6.php";
