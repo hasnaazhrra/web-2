25304f4d1dd6bff255fb45c0dd490db5586e2688
hasnaazhrra <hasnaazzahera@gmail.com>
2025-05-29 23:22:21 +0700

    praktikum10

--- views/partials/navbar.php
+++
@@ -1,8 +0,0 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand ps-3" href="index.php">Koperasi Pegawai</a>
-    <ul class="navbar-nav ms-auto me-3">
-        <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fas fa-user-circle"></i> Admin</a>
-        </li>
-    </ul>
-</nav>
